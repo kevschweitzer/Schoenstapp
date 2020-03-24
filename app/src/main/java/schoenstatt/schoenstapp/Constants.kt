b69e7ae2ed152d4d2861e018f73ab357412e411b
Kevin <kevschweitzer@gmail.com>
2020-03-24 20:08:55 -0300

    Share/Join capital change

--- app/src/main/java/schoenstatt/schoenstapp/Constants.kt
+++ app/src/main/java/schoenstatt/schoenstapp/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
     companion object {
         const val SINGLE_CAPITAL = "single_capital"
         const val APPLINK_DATA = "applink_data"
-        const val BASE_SHARE_URL = "https://kevschweitzer.github.io/schoenstapp-share/"
+        const val BASE_SHARE_URL = "https://kevschweitzer.github.io/schoenstappweb"
     }
 }
